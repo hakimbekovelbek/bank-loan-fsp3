6085bb3d6fb78dfb28dab21540a98c39db4115ed
Bobur Bazarbayev <bozorboyevbobur2004@gmail.com>
2021-11-15 14:41:13 +0500

    13.11.2021

---
+++ Desktop/Class work/ClassWork_13_11_2021.py
@@ -0,0 +1,25 @@
+# num1 = int(input('Enter 1st number:'))
+# # num2 = int(input('Enter 2nd number:'))
+# # print(f'{num1} - {num2} = {num1 - num2}')
+# # print(f'{num1} - {num2} = {num1 - num2}')
+
+# # print('I\'m Bobur')
+# # print("I'm \n Bobur")
+# # print("I'm \t Bobur")
+
+# # print(True and False)
+# # print(True or False)
+# # print( 1 or False)
+# # print( 20 or False)
+# # print( 0 or False)
+# # print( 0 or False or 5)
+# # print( 'Elbek' or False)
+# # print( '' or False)
+# # print( 2 or 55)
+# # print( 2 or 55 and False)
+# # print( 2 and 4 or False and 22 )
+
+# login = 'admin'
+# password = "admin"
+# if login == "admin" and password == "admin": print('Welcome')
+# else: print('Bye')
